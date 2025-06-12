554e36b8e1c69252280898d77a349b20ba1ac0ec
protapkrghosh <protapg518@gmail.com>
2025-06-12 11:37:30 +0600

    update not found page

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router";
+import image from '../assets/404.png'
 
 const PageNotFound = () => {
    return (
-      <div className="min-h-screen text-center mt-32">
-         <h1 className="text-6xl font-bold tracking-wider rancho capitalize">404 • Page not found</h1>
+      <div className="min-h-screen text-center py-20">
+         <img src={image} alt="Image" className="w-10/12 mx-auto mb-10" />
+         <h1 className="text-6xl font-bold tracking-wider rancho capitalize">
+            OOPS! the page was not found
+         </h1>
          <p className="md:w-[50%] mx-auto my-9">
-            Oops! The page you are looking for does not exist. It might have
+            The page you are looking for does not exist. It might have
             been moved or deleted. Try a search below...
          </p>
 
