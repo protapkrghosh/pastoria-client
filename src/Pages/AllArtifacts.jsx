a0cf6f0bb923d1588e5a4492801c5ad5c662eaa2
protapkrghosh <protapg518@gmail.com>
2025-06-17 00:47:35 +0600

    added search input in all artifact page

--- src/Pages/AllArtifacts.jsx
+++ src/Pages/AllArtifacts.jsx
@@ -12,7 +12,7 @@ const AllArtifacts = () => {
          </Helmet>
 
          <div className="container mx-auto px-2 md:px-6 lg:px-12 py-16 bg-base-200">
-            <div className="text-secondary mb-10">
+            <div className="text-secondary">
                <h1 className="rancho text-5xl font-bold text-center tracking-wider opacity-70 mb-5">
                   All Historical Artifacts
                </h1>
@@ -23,6 +23,29 @@ const AllArtifacts = () => {
                   civilizations, beliefs, and artistry of those who came before
                   us
                </p>
+            </div>
+
+            {/* Search */}
+            <div className="my-10 w-3/5 mx-auto">
+               <label className="input cusInput">
+                  <svg
+                     className="h-[1em] opacity-50"
+                     xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 24 24"
+                  >
+                     <g
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        strokeWidth="2.5"
+                        fill="none"
+                        stroke="currentColor"
+                     >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                     </g>
+                  </svg>
+                  <input type="search" required placeholder="Search Artifact" />
+               </label>
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
