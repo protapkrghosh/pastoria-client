2f247d0939d119e027ff65becc9dcf3dfe2dc6f8
protapkrghosh <protapg518@gmail.com>
2025-06-17 05:34:10 +0600

    update liked page

--- src/Pages/LikedArtifacts.jsx
+++ src/Pages/LikedArtifacts.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useLoaderData, Link } from "react-router";
 import { AuthContext } from "../Context/AuthProvider";
 import { FaHeart, FaRegSadTear } from "react-icons/fa";
+import arrow from "../assets/arrow.png";
+import { FiLink } from "react-icons/fi";
 
 const LikedArtifacts = () => {
    const data = useLoaderData();
@@ -24,48 +26,59 @@ const LikedArtifacts = () => {
             </h2>
 
             {likedArtifacts.length === 0 ? (
-               <div className="flex flex-col items-center justify-center text-center mt-28">
-                  <FaRegSadTear className="text-5xl text-gray-400 mb-4" />
+               <div className="flex flex-col items-center justify-center text-center mt-12">
+                  <FaRegSadTear className="text-5xl text-primary mb-4" />
                   <h3 className="text-2xl font-semibold text-gray-700">
                      No Liked Artifacts Found
                   </h3>
                   <p className="text-gray-500 mt-2">
-                     You haven’t expressed love for history yet!
+                     You haven’t expressed liked for history yet!
                   </p>
                </div>
             ) : (
-               <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10">
+               <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
                   {likedArtifacts.map((artifact) => (
                      <div
                         key={artifact._id}
-                        className="relative bg-white/70 shadow-xl border border-slate-200 rounded-3xl overflow-hidden group"
+                        className="bg-white/70 hover:shadow-md border border-accent rounded-md overflow-hidden duration-300"
                      >
-                        {/* Image */}
-                        <img
-                           src={artifact?.imageURL}
-                           alt={artifact?.artifactName}
-                           className="w-full h-56 object-cover transition-transform duration-500 group-hover:scale-105"
-                        />
+                        {/* Image Area with Hover */}
+                        <div className="relative group">
+                           <img
+                              src={artifact?.imageURL}
+                              alt={artifact?.artifactName}
+                              className="w-full h-56 object-cover transition-transform duration-500 group-hover:scale-105"
+                           />
+                           <div className="absolute inset-0 bg-black/80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center h-[230px]">
+                              <img
+                                 src={arrow}
+                                 alt="Icon"
+                                 className="w-1/2 h-2"
+                              />
 
-                        {/* Hover Overlay */}
-                        <div className="absolute inset-0 bg-black/60 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                           <Link
-                              to={`/artifact/${artifact._id}`}
-                              className="bg-white text-black px-4 py-2 rounded-full font-semibold text-sm hover:bg-slate-100 shadow-md transition"
-                           >
-                              View Details
-                           </Link>
+                              <Link
+                                 to={`/artifact/${artifact._id}`}
+                                 className="bg-primary/80 p-2 mt-4"
+                              >
+                                 <FiLink size={17} className="text-white" />
+                              </Link>
+                           </div>
                         </div>
 
-                        {/* Info */}
+                        {/* Info Section */}
                         <div className="p-5 space-y-2">
-                           <div className="flex items-center gap-2 pt-1">
-                              <FaHeart className="text-red-500 text-sm" />
-                              <span className="text-sm text-slate-600">
-                                 {artifact.likedBy?.length || 0} Likes
-                              </span>
+                           <div className="text-sm text-secondary flex items-center gap-2 pt-1">
+                              <div>
+                                 <span className="font-semibold">
+                                    {artifact.likedBy?.length || 0}
+                                 </span>{" "}
+                                 Likes
+                              </div>
+
+                              <div>{artifact?.presentLocation}</div>
                            </div>
-                           <h3 className="text-md font-semibold text-slate-800 truncate">
+
+                           <h3 className="ptSerif text-base font-semibold text-black tracking-wide line-clamp-2">
                               {artifact?.artifactName}
                            </h3>
                         </div>
